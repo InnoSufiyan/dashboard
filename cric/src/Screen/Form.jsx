e38d246c3d8d148be6f9fea89fbf8cbbaea38f87
inno sufiyan <innosufiyan@gmail.com>
2021-11-18 05:16:02 -0800

    two

--- cric/src/Screen/Form.jsx
+++ cric/src/Screen/Form.jsx
@@ -4,7 +4,19 @@ import axios from "axios"
 import { baseURI } from './core'
 const Form = () => {
     const [seriesName , setSeriesName] = useState(null)
- 
+    const [teamOneName , setTeamOneName] = useState(null)
+    const [teamTwoName , setTeamTwoName] = useState(null)
+    const [teamOneScore , setTeamOneScore] = useState(null)
+    const [teamTwoScore , setTeamTwoScore] = useState(null)
+    const [btOne , setBtOne] = useState(null)
+    const [btTwo , setBtTwo] = useState(null)
+    const [btOneScore , setBtOneScore] = useState(null)
+    const [btTwoScore , setBtTwoScore] = useState(null)
+    
+    const [bowOne , setBowOne] = useState(null)
+    const [bowTwo , setBowTwo] = useState(null)
+    const [bowTwoScore , setBowTwoScore] = useState(null)
+    const [bowOneScore , setBowOnescore] = useState(null)
     const submitHandler = ()=>{
         console.log("hit")
 
@@ -26,10 +38,12 @@ const Form = () => {
             <input type="text" placeholder="Team 2 score" id="" />
             <input type="text" placeholder="Batting Player 1" id="" />
             <input type="text" placeholder="Batting Player 2" id="" />
-            <input type="text" placeholder="Batting Player 2" id="" />
-            <input type="text" placeholder="Batting Player 2" id="" />
+            <input type="text" placeholder="Batting Player 1 Score" id="" />
+            <input type="text" placeholder="Batting Player 2 Score" id="" />
             <input type="text" placeholder="Bowler Player 1" id="" />
             <input type="text" placeholder="Bowler Player 2" id="" />
+            <input type="text" placeholder="Bowler Player Stats 1" id="" />
+            <input type="text" placeholder="Bowler Player Stats 2" id="" />
 
             <button onClick={submitHandler}>SUBMIT</button>
 
